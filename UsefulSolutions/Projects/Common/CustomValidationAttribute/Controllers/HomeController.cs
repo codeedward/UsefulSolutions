ade60f4cc935f8eedffc6f2bfb676e94a9c677b4
Adam Mocarski <mocarski.contact@gmail.com>
2018-04-02 23:23:46 +0100

    Custom validation attribute using default mvc validation. Setting up default mvc validation first.

--- UsefulSolutions/Projects/Common/CustomValidationAttribute/Controllers/HomeController.cs
+++ UsefulSolutions/Projects/Common/CustomValidationAttribute/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CustomValidationAttribute.Models;
 
 namespace CustomValidationAttribute.Controllers
 {
@@ -10,7 +11,18 @@ namespace CustomValidationAttribute.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new SomeModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(SomeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View(model);
         }
 
         public ActionResult About()
---
+++ UsefulSolutions/Projects/Common/CustomValidationAttribute/Models/SomeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomValidationAttribute.Models
+{
+    public class SomeModel
+    {
+        public string NoValidationField { get; set; }
+
+        [Required]
+        public string RequiredField { get; set; }
+
+        [DataType(DataType.Date)]
+        public string DateTime { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+    }
+}
