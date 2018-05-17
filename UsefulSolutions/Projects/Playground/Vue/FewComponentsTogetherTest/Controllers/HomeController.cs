878813bc59f1023f64f284fd887a4203d394e7f6
codeedward <mocarski.contact@gmail.com>
2018-05-17 23:13:49 +0100

    Prepare vue.js and main root.

--- UsefulSolutions/Projects/Playground/Vue/FewComponentsTogetherTest/Controllers/HomeController.cs
+++ UsefulSolutions/Projects/Playground/Vue/FewComponentsTogetherTest/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace FewComponentsTogetherTest.Controllers
 
             return View();
         }
+
+        public ActionResult VueTest()
+        {
+            return View();
+        }
     }
 }
